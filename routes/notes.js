082d717c0b0b7bcef5c5468a014b0d08ecffa78f
Aleksandra <a.krakowiak@stud.elka.pw.edu.pl>
2021-06-10 21:49:27 +0200

    znalezienie bledu

--- routes/notes.js
+++ routes/notes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router()
 
-const isAuthenticated = require('../config/auth');
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 //Note model
 const Note = require('../models/Note');
 
-router.get('/add', isAuthenticated, (req, res) => res.render('new'))
+router.get('/add', ensureAuthenticated, (req, res) => res.render('new'))
 
-//router.get('/add', isAuthenticated, (req,res) => res.render('new'));
+//router.get('/add', ensureAuthenticated, (req,res) => res.render('new'));
 
-router.post('/new', isAuthenticated, (req,res)=>{
+router.post('/new', ensureAuthenticated, (req,res)=>{
     const {title, description} = req.body
     let errors = []
     if(!title){
@@ -37,24 +37,24 @@ router.post('/new', isAuthenticated, (req,res)=>{
 })
 
 // All notes
-router.get('/', isAuthenticated, (req, res) =>{
+router.get('/', ensureAuthenticated, (req, res) =>{
     const notes = Note.find({user: req.user.login}).sort({date: 'desc'})
     res.render('all', { notes })
 })
 
-router.get('/edit/:id', isAuthenticated, (req, res)=>{
+router.get('/edit/:id', ensureAuthenticated, (req, res)=>{
     const note = Note.findById(req.params.id)
     res.render('/notes/edit', {note})
 })
 
-router.put('/edit/:id', isAuthenticated, (req,res) => {
+router.put('/edit/:id', ensureAuthenticated, (req,res) => {
     const {title, description} = req.body
     Note.findByIdAndUpdate(req.params.id, {title, description})
     req.flash('success_msg', 'Note Updated Successfully')
     res.redirect('/notes')
 })
 
-router.delete('/delete/:id', isAuthenticated, (req,res)=>{
+router.delete('/delete/:id', ensureAuthenticated, (req,res)=>{
     Note.findByIdAndDelete(req.params.id)
     req.flash('success_msg', 'Note Deleted Successfully')
     res.redirect('/notes')
