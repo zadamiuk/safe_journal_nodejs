18220c03e0f6126e5b6d2e221c03bd1d9029a300
jrancew <j.rancew@stud.elka.pw.edu.pl>
2021-06-13 22:17:37 +0200

    zmiana instrukcji logowania

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # BEMSI_notatnik
 
-## Please install:
+## Przed uruchomieniem, proszę zainstalować:
 npm install
 npm i express bcryptjs passport passport-local ejs express-ejs-layouts mongoose connect-flash express-session mongodb   
 npm i crypto buffer   
@@ -9,6 +9,6 @@ npm i -D nodemon
 
 ### Aplikacje uruchamiamy poleceniem: npm run start
 
-#### przykladowe konto do sprawdzania:
-login: bemsi
-hasło: bemsibemsi
+#### przykladowe konto do sprawdzania działania:  
+login: uzytkownik1  
+hasło: K8QnZBu?AR_#FP4d  
--- routes/notes.js
+++ routes/notes.js
@@ -15,8 +15,8 @@ const iv = crypto.randomBytes(16); // iv generated randomly
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 //  Encryption function
-function encrypt(text,key1,iv1) {
-    
+function encrypt(text, key1, iv1) {
+
     const cipher = crypto.createCipheriv(algorithm, key1, iv1);
 
     const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
@@ -54,43 +54,43 @@ router.get('/new', ensureAuthenticated, (req, res) => res.render('new'))
 
 
 // New Note
-router.post('/new', ensureAuthenticated, (req,res)=>{
-    const {title, description} = req.body
+router.post('/new', ensureAuthenticated, (req, res) => {
+    const { title, description } = req.body
     let errors = [];
-    if(!title){
+    if (!title) {
         errors.push({ msg: 'Please insert a title.' });
     }
 
     if (!description) {
-        errors.push({ msg: 'Please insert a description' }); 
+        errors.push({ msg: 'Please insert a description' });
     }
 
-    if(errors.length > 0){
+    if (errors.length > 0) {
         res.render('new', {
-            errors, 
-            title, 
-            description 
+            errors,
+            title,
+            description
         });
     } else {
 
-    // encryption
-        const encrypteddDescription=encrypt(description,key,iv);
-        
+        // encryption
+        const encrypteddDescription = encrypt(description, key, iv);
+
 
         console.log(encrypteddDescription);
 
-    // decryption for testing 
-        var note1=decrypt(encrypteddDescription.content,key,encrypteddDescription.iv);
+        // decryption only for testing 
+        var note1 = decrypt(encrypteddDescription.content, key, encrypteddDescription.iv);
         console.log("Odszyfrowane:" + note1);
 
         // adding to database
 
-        const newNote = new Note({title, description: encrypteddDescription.content}) 
+        const newNote = new Note({ title, description: encrypteddDescription.content })
         newNote.user = req.user.login
         newNote.save()
 
         //creating a key and adding to database - NOT EVERYTHING CORRECT - iv and key 
-        const newKey = new Key({value: key, iv: iv, _id_note: newNote._id});
+        const newKey = new Key({ value: key, iv: iv, _id_note: newNote._id });
         newKey.user = req.user.login
         newKey.save()
         res.redirect('/notes/all');
@@ -98,98 +98,99 @@ router.post('/new', ensureAuthenticated, (req,res)=>{
 })
 
 // All notes
-router.get('/all', ensureAuthenticated,  (req, res) => {
+router.get('/all', ensureAuthenticated, (req, res) => {
 
 
 
-// CODE BELOW: NOT EVERYTHING CORRECT
-//  - A PART OF CODE TO COMMUNICATE WITH DATABASE TO STORE KEYS SAFELY
+    // CODE BELOW: NOT EVERYTHING CORRECT
+    //  - A PART OF CODE TO COMMUNICATE WITH DATABASE TO STORE KEYS SAFELY
 
-//   Note.find({user: req.user.login}, function(err,data) { 
-    
-//      if(err){
-//          console.log(err);
-//          res.send(500).status;
+    //   Note.find({user: req.user.login}, function(err,data) { 
 
-//      }
-//      else {
-//         const secret=Key.findOne({user: req.user.login, 
-//             title: data.title})
+    //      if(err){
+    //          console.log(err);
+    //          res.send(500).status;
 
-//         // .catch(err => {
-//         //     console.log(err);
-//         //     res.send(500).status;
-//         // });
-     
-//         secret.select('key iv');
-//         secret.exec(function(err, secretSet){
-//          console.log('key: %s, iv: %s',secretSet.key, secretSet.iv);
-//         });
+    //      }
+    //      else {
+    //         const secret=Key.findOne({user: req.user.login, 
+    //             title: data.title})
 
-//         await Key.findOne({ user: req.user.login, 
-//             title: data.title }, 'key iv').exec();
+    //         // .catch(err => {
+    //         //     console.log(err);
+    //         //     res.send(500).status;
+    //         // });
 
-//         console.log(data);
+    //         secret.select('key iv');
+    //         secret.exec(function(err, secretSet){
+    //          console.log('key: %s, iv: %s',secretSet.key, secretSet.iv);
+    //         });
 
-        
-//    res.render('all.ejs', {Note: data});
-//      }
+    //         await Key.findOne({ user: req.user.login, 
+    //             title: data.title }, 'key iv').exec();
 
-                    
-//    })
-   
-// })
-   
+    //         console.log(data);
 
 
+    //    res.render('all.ejs', {Note: data});
+    //      }
 
-// commented code - not everything is correct 
- Note.find({user: req.user.login}, function(err,data) { 
-    
-     if(err){
-         console.log(err);
-         res.send(500).status;
-     }
-     else {
-        // data.forEach( function( data1){
-        //     // Key.find({user: req.user.login, title:data.title}, function(err,data2) {
-        //     //     if(err){
-        //     //         console.log(err);
-        //     //         res.send(500).status;
-        //     //     }else{
-        //     //         console.log(data2.key, data2.iv);
-                
-        //     //         //data1.description=decrypt(data1.description, data2.key, data2.iv)
-        //     //     }
-        //     //  })})
-        res.render('all.ejs', {
-        Note: data});
-        }            
-   });
- })
 
- 
+    //    })
+
+    // })
+
+
+
+
+    // commented code - not everything is correct 
+    Note.find({ user: req.user.login }, function (err, data) {
+
+        if (err) {
+            console.log(err);
+            res.send(500).status;
+        }
+        else {
+            // data.forEach( function( data1){
+            //     // Key.find({user: req.user.login, title:data.title}, function(err,data2) {
+            //     //     if(err){
+            //     //         console.log(err);
+            //     //         res.send(500).status;
+            //     //     }else{
+            //     //         console.log(data2.key, data2.iv);
+
+            //     //         //data1.description=decrypt(data1.description, data2.key, data2.iv)
+            //     //     }
+            //     //  })})
+            res.render('all.ejs', {
+                Note: data
+            });
+        }
+    });
+})
+
+
 // // UNUSED PART BEL0W:
 //  router.post('/all', ensureAuthenticated, (req, res) =>{
 //      var notes = Note.find({user: req.user.login}).sort({date: 'desc'})
 //      if(notes) {
 
-        
+
 //          res.render('all', { notes })
 //      } else {
 //           //to raczej nie działa/nie jest uruchamiane(?)
 //          res.render('no_notes')
 //      }
 //  })
- 
+
 // UNUSED PART BELOW:
-router.get('/edit/:id', ensureAuthenticated, (req, res)=>{
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     const note = Note.findById(req.params.id)
-    res.render('/notes/edit', {note})
+    res.render('/notes/edit', { note })
 })
 
 // UNUSED PART BELOW:
-router.put('/edit/:id', ensureAuthenticated, (req,res) => {
+router.put('/edit/:id', ensureAuthenticated, (req, res) => {
     // const {title, description} = req.body
     // Note.findByIdAndUpdate(req.params.id, {title, description})
     // req.flash('success_msg', 'Note Updated Successfully')
@@ -205,9 +206,9 @@ router.put('/edit/:id', ensureAuthenticated, (req,res) => {
     // find and update
     Note.findByIdAndUpdate(req.params.id, {
         $set: req.body
-    }, {new: true})
+    }, { new: true })
         .then(data => {
-            if(!data) {
+            if (!data) {
                 return res.status(404).send({
                     success: false,
                     message: "Note not found with id " + req.params.id
@@ -218,43 +219,43 @@ router.put('/edit/:id', ensureAuthenticated, (req,res) => {
                 data: data
             });
         }).catch(err => {
-        if(err.kind === 'ObjectId') {
-            return res.status(404).send({
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    success: false,
+                    message: "Note not found with id " + req.params.id
+                });
+            }
+            return res.status(500).send({
                 success: false,
-                message: "Note not found with id " + req.params.id
+                message: "Error updating note with id " + req.params.id
             });
-        }
-        return res.status(500).send({
-            success: false,
-            message: "Error updating note with id " + req.params.id
-        });
-    })
+        })
 })
 
 // Delete
-router.get('/delete/:id', ensureAuthenticated, function(req, res) {
+router.get('/delete/:id', ensureAuthenticated, function (req, res) {
     Note.findByIdAndRemove(req.params.id, function (err, project) {
-      if (err) {
-        req.flash('errorMsg', 'User not deleted successfully.');
-        res.redirect('/notes/all');
-      } else {
-        req.flash('successMsg', 'User deleted successfully.');
-        res.redirect('/notes/all');
-      }
+        if (err) {
+            req.flash('errorMsg', 'User not deleted successfully.');
+            res.redirect('/notes/all');
+        } else {
+            req.flash('successMsg', 'User deleted successfully.');
+            res.redirect('/notes/all');
+        }
     });
-  });
-  
-  router.get('/note/:id', ensureAuthenticated, function(req, res) {
-    Key.findOne({_id_note: req.params.id},function (err, key) {
-    
-        if(err){
+});
+
+router.get('/note/:id', ensureAuthenticated, function (req, res) {
+    Key.findOne({ _id_note: req.params.id }, function (err, key) {
+
+        if (err) {
             console.log(err);
             res.send(500).status;
         }
-        else{
+        else {
             Note.findById(req.params.id, function (err, data) {
-    
-                if(err){
+
+                if (err) {
                     console.log(err);
                     res.send(500).status;
                 }
@@ -262,21 +263,21 @@ router.get('/delete/:id', ensureAuthenticated, function(req, res) {
                     console.log(data.description);
                     console.log("key:  ", key.value);
                     console.log("iv:  ", key.iv);
-                   const text = decrypt(data.description, key.value, key.iv) 
-                   console.log(1);
-                   console.log(text);
-                   res.send(text)
-                   //res.render('note.ejs', {
-                   //Note: data});
-                   }            
-              });
-           
+                    const text = decrypt(data.description, key.value, key.iv)
+                    console.log(1);
+                    console.log(text);
+                    res.send(text)
+                    //res.render('note.ejs', {
+                    //Note: data});
+                }
+            });
+
         }
     })
     //res.send(req.params.id)
-    
-        })
-       ;
-  
+
+})
+    ;
+
 
 module.exports = router;
